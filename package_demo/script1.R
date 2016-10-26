89f0535cd0c9178925b8a45ab8d9913734b4bd7c
Thomas Girke <tgirke@citrus.ucr.edu>
2016-10-25 20:34:10 -0700

    new files

---
+++ package_demo/script1.R
@@ -0,0 +1,13 @@
+#####################################################
+## Function to compute column-wise aggregate stats ##
+#####################################################
+myMAcomp <- function(myMA=myMA, group=c(1,1,1,2,2,2,3,3,4,4), myfct=mean) {
+    myList <- tapply(colnames(myMA), group, list)
+    names(myList) <- sapply(myList, paste, collapse="_")
+    myMAmean <- sapply(myList, function(x) apply(myMA[, x, drop=FALSE], 1, myfct))
+    return(myMAmean)
+}
+## Usage: 
+# myMA <- matrix(rnorm(100000), 10000, 10, dimnames=list(1:10000, paste("C", 1:10, sep="")))
+# myMAcomp(myMA=myMA, group=c(1,1,1,2,2,2,3,3,4,4), myfct=mean)[1:2,] 
+
